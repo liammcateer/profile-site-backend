bc36d3ab4214e323fce8e6edefaeb727160ea777
Liam McAteer <liam@liammcateer.nz>
2020-07-18 21:51:39 +1200

    removed hello world from root endpoint

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var apiRouter = require('./api/apiRouter')
 
 app.use(cors())
 app.use(express.json())
-app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/api', apiRouter)
 
 app.listen(port, () => console.log("App listening on port " + port))
